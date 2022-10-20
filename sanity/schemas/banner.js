3df4cacb2b16e2b725353db00ece1cbd16b95d86
Beatriz Norbiato <bnorbiatos@gmail.com>
2022-10-20 14:53:15 -0300

    Create schemas for product and banner

---
+++ sanity/schemas/banner.js
@@ -0,0 +1,55 @@
+export default {
+    name: 'banner',
+    title: 'Banner',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: 'buttonText',
+            title: 'Button text',
+            type: 'string',
+        },
+        {
+            name: 'product',
+            title: 'Product',
+            type: 'string',
+        },
+        {
+            name: 'desc',
+            title: 'Desc',
+            type: 'string',
+        },
+        {
+            name: 'smallText',
+            title: 'Small text',
+            type: 'string',
+        },
+        {
+            name: 'middleText',
+            title: 'Middle text',
+            type: 'string',
+        },
+        {
+            name: 'largeText',
+            title: 'Large text',
+            type: 'string',
+        },
+        {
+            name: 'discount',
+            title: 'Discount',
+            type: 'string',
+        },
+        {
+            name: 'saleTime',
+            title: 'SaleTime',
+            type: 'string',
+        },
+    ],
+};
